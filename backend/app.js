5e254f20c64fd7d5d27cf09ddc70fb83d21a4e89
SharmaYashasvi <202011079@diu.iiitvadodara.ac.in>
2023-04-25 17:21:22 +0530

    jai shri ram

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
+app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(cookieParser());
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
 // route import 
 const user = require("./routes/userRoute")
 
 app.use("/api/v1",user);
 
 module.exports = app ;
+
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+  )
+);
